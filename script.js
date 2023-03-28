b97c6b994ae93ed45a1deb8166f5883c0a3c9522
Cheikh Ahmed Tidiane Gueye <cheikhahmedtidiane219@gmail.com>
2023-03-28 13:11:25 +0000

    Cheikh

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ window.addEventListener('scroll', function() {
     navbar.classList.remove('scroll');
   }
 });
+
+// Récupérer tous les liens dans la barre de navigation
+var navLinks = document.querySelectorAll('#navbar .navbar-nav a');
+
+// Ajouter un écouteur d'événement click à chaque lien
+navLinks.forEach(function(link) {
+  link.addEventListener('click', function() {
+    // Fermer l'offcanvas en définissant son attribut aria-hidden à true
+    var offcanvas = document.querySelector('#offcanvasExample');
+    offcanvas.setAttribute('aria-hidden', 'true');
+    
+    // Si le lien cliqué est le lien qui ouvre l'offcanvas, réinitialiser son attribut aria-expanded à false
+    var menuToggler = document.querySelector('#menu-toggler');
+    if (link === menuToggler) {
+      menuToggler.setAttribute('aria-expanded', 'false');
+    }
+  });
+});
+
+
